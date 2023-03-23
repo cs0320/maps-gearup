955b8657916a7aac56e6e8728f13e05264c8533c
obloomfield <obloomfield@brown.edu>
2023-03-23 18:49:40 -0400

    solution stencil

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,57 @@
+import "mapbox-gl/dist/mapbox-gl.css";
 import React, { useEffect, useState } from "react";
-import "./App.css";
 
-// REMEMBER TO PUT YOUR API KEY IN A FOLDER THAT IS GITIGNORED!!
-// (for instance, /src/private/api_key.tsx)
-// import {API_KEY} from "./private/api_key"
+import Map, { Layer, MapLayerMouseEvent, Source } from "react-map-gl";
+import "./App.css";
+import { geoLayer, overlayData } from "./overlays";
+
+// You need to make this private file api.ts yourself!
+import { ACCESS_TOKEN } from "./private/api";
+
+interface LatLong {
+  lat: number;
+  long: number;
+}
+
+function onMapClick(e: MapLayerMouseEvent) {
+  console.log(e.lngLat.lat);
+  console.log(e.lngLat.lng);
+}
 
 function App() {
-  return <div className="App">
-    {/* TODO: PLACE MAP CONTENT HERE */}
-  </div>
+  const ProvidenceLatLong: LatLong = { lat: 41.824, long: -71.4128 };
+  const initialZoom = 10;
+
+  const [viewState, setViewState] = useState({
+    longitude: ProvidenceLatLong.long,
+    latitude: ProvidenceLatLong.lat,
+    zoom: initialZoom,
+  });
+
+  const [overlay, setOverlay] = useState<GeoJSON.FeatureCollection | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    setOverlay(overlayData());
+  }, []);
+
+  return (
+    <div className="App">
+      <Map
+        mapboxAccessToken={ACCESS_TOKEN}
+        {...viewState}
+        onMove={(ev) => setViewState(ev.viewState)}
+        style={{ width: window.innerWidth, height: window.innerHeight }}
+        mapStyle={"mapbox://styles/mapbox/dark-v10"}
+        onClick={(ev: MapLayerMouseEvent) => onMapClick(ev)}
+      >
+        <Source id="geo_data" type="geojson" data={overlay}>
+          <Layer {...geoLayer} />
+        </Source>
+      </Map>
+    </div>
+  );
 }
 
 export default App;
