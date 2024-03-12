b284e7c8f9d5886054c56b59706532cea66521df
jmaffa <90526195+jmaffa@users.noreply.github.com>
2024-03-12 15:50:25 -0400

    playtesting branch

--- client/src/utils/cookie.ts
+++ client/src/utils/cookie.ts
@@ -4,14 +4,18 @@ import Cookies from "js-cookie";
 // - Fill in these functions to add cookie functionality to the firebase login.
 
 export function addLoginCookie(uid: string): void {
+  Cookies.add("uid", uid);
   // TODO: fill out!
 }
 
 export function removeLoginCookie(): void {
+  Cookies.remove("uid");
   // TODO: fill out!
 }
 
 export function getLoginCookie(): string | undefined {
+  const loginCookie = Cookies.get("uid");
+  return loginCookie;
   // TODO: fill out!
   return undefined;
 }
